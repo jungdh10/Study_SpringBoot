3125fc4681650e97c580a7349c47f41bc465a31e
jungdh10 <dhh0089@gmail.com>
2023-12-21 17:56:51 +0900

    회원 서비스 개발(전체 회원 조회 등)

--- hello-spring/src/main/java/hello/hellospring/Service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/Service/MemberService.java
@@ -4,6 +4,7 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
@@ -26,4 +27,17 @@ public class MemberService {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         });
     }
+
+
+    /**
+     *전체 회원 조회 */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+
 }
